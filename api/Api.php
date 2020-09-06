369bed473f3c45d720c43c87093a38cf951c5978
Minerva-07 <dikaiostheo@gmail.com>
2020-09-06 14:06:00 +0700

    first commit

---
+++ api/Api.php
@@ -0,0 +1,140 @@
+<?php
+
+//Api.php
+
+class API
+{
+	private $connect = '';
+
+	function __construct()
+	{
+		$this->database_connection();
+	}
+
+	function database_connection()
+	{
+		$this->connect = new PDO("mysql:host=localhost;dbname=gkps", "root", "root123");
+	}
+
+	function fetch_all()
+	{
+		$query = "SELECT * FROM gereja ORDER BY id";
+		$statement = $this->connect->prepare($query);
+		if($statement->execute())
+		{
+			while($row = $statement->fetch(PDO::FETCH_ASSOC))
+			{
+				$data[] = $row;
+			}
+			return $data;
+		}
+	}
+
+	function insert()
+	{
+		if(isset($_POST["name"]))
+		{
+			$form_data = array(
+				':name'		=>	$_POST["name"],
+				':distrik'		=>	$_POST["distrik"]
+			);
+			$query = "
+			INSERT INTO gereja 
+			(name, distrik) VALUES 
+			(:name, :distrik)
+			";
+			$statement = $this->connect->prepare($query);
+			if($statement->execute($form_data))
+			{
+				$data[] = array(
+					'success'	=>	'1'
+				);
+			}
+			else
+			{
+				$data[] = array(
+					'success'	=>	'0'
+				);
+			}
+		}
+		else
+		{
+			$data[] = array(
+				'success'	=>	'0'
+			);
+		}
+		return $data;
+	}
+
+	function fetch_single($id)
+	{
+		$query = "SELECT * FROM gereja WHERE id='".$id."'";
+		$statement = $this->connect->prepare($query);
+		if($statement->execute())
+		{
+			foreach($statement->fetchAll() as $row)
+			{
+				$data['name'] = $row['name'];
+				$data['distrik'] = $row['distrik'];
+			}
+			return $data;
+		}
+	}
+
+	function update()
+	{
+		if(isset($_POST["name"]))
+		{
+			$form_data = array(
+				':name'	=>	$_POST['name'],
+				':distrik'	=>	$_POST['distrik'],
+				':id'			=>	$_POST['id']
+			);
+			$query = "
+			UPDATE gereja 
+			SET name = :name, distrik = :distrik 
+			WHERE id = :id
+			";
+			$statement = $this->connect->prepare($query);
+			if($statement->execute($form_data))
+			{
+				$data[] = array(
+					'success'	=>	'1'
+				);
+			}
+			else
+			{
+				$data[] = array(
+					'success'	=>	'0'
+				);
+			}
+		}
+		else
+		{
+			$data[] = array(
+				'success'	=>	'0'
+			);
+		}
+		return $data;
+	}
+	function delete($id)
+	{
+		$query = "DELETE FROM gereja WHERE id = '".$id."'";
+		$statement = $this->connect->prepare($query);
+		if($statement->execute())
+		{
+			$data[] = array(
+				'success'	=>	'1'
+			);
+		}
+		else
+		{
+			$data[] = array(
+				'success'	=>	'0'
+			);
+		}
+		return $data;
+	}
+}
+
+?>
